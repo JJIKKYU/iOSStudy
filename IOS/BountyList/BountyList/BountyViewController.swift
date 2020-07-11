11f2e4d0b092fc595136ece55404caa9a1c14765
Jin Gyun, Jeong <jjinggu96@gmail.com>
2020-07-11 18:15:20 +0900

    Update : IOS TableView Segue Function Study
    테이블 뷰를 어느정도 완성하고 내부에 있는 Cell을 클릭했 을때 넘어가도록 함.

--- IOS/BountyList/BountyList/BountyViewController.swift
+++ IOS/BountyList/BountyList/BountyViewController.swift
@@ -10,38 +10,48 @@ import UIKit
 
 class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    let NameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
+    let BountyList = [330000, 50, 440000, 300000, 160000, 800000, 770000, 120000]
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-    // UITableView DataSource
-    
+    // UITableView DataSource    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2;
+        return BountyList.count;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        return cell;
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+            // 캐스팅이 안 된 경우
+            return UITableViewCell()
+        }
+        
+        let img = UIImage(named: "\(NameList[indexPath.row]).jpg")
+        cell.imageView?.image = img
+        cell.NameLabel.text = NameList[indexPath.row]
+        cell.BountyLabel.text = "\(BountyList[indexPath.row])"
+        return cell
     }
     
     // UITableViewDelegate
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        performSegue(withIdentifier: "ShowDetail", sender: nil)
+        
         print("--> \(indexPath.row)")
     }
+}
+
+class ListCell : UITableViewCell {
+    @IBOutlet weak var ImageView : UIImageView!
+    @IBOutlet weak var NameLabel : UILabel!;
+    @IBOutlet weak var BountyLabel : UILabel!;
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func ListCell() {
+        
     }
-    */
-
 }
---
+++ IOS/BountyList/BountyList/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  BountyList
+//
+//  Created by JJIKKYU on 2020/07/11.
+//  Copyright © 2020 JJIKKYU. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func Close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
