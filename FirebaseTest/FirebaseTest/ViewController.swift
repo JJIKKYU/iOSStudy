242b1e4afbe44d18a7a50ea70568d64efcf6fc1f
Jin Gyun, Jeong <jjinggu96@gmail.com>
2020-08-06 22:41:32 +0900

    UPDATE : Firebase 연동

--- FirebaseTest/FirebaseTest/ViewController.swift
+++ FirebaseTest/FirebaseTest/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    let db = Database.database().reference()
 
+    @IBOutlet weak var dataLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateLabel()
+    }
+    
+    func updateLabel() {
+        db.child("firstData").observeSingleEvent(of: .value) { snapshot in
+            print(snapshot)
+            let value = snapshot.value as? String ?? ""
+            DispatchQueue.main.async {
+                self.dataLabel.text = value
+            }
+            
+            
+        }
     }
 
 
