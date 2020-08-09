3664b9b2ec24f94f2286008785f3968985890b24
Jin Gyun, Jeong <jjinggu96@gmail.com>
2020-08-09 18:34:36 +0900

    UPDATE : Save Image

--- FullScreenCamera/FullScreenCamera/CameraViewController.swift
+++ FullScreenCamera/FullScreenCamera/CameraViewController.swift
@@ -26,7 +26,7 @@ class CameraViewController: UIViewController {
     let sessionQueue = DispatchQueue(label: "session Queue")
     let videoDeviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera, .builtInTripleCamera, .builtInWideAngleCamera, .builtInTrueDepthCamera], mediaType: .video, position: .unspecified)
     
-
+    
     @IBOutlet weak var photoLibraryButton: UIButton!
     @IBOutlet weak var previewView: PreviewView!
     @IBOutlet weak var captureButton: UIButton!
@@ -160,18 +160,49 @@ class CameraViewController: UIViewController {
         }
         
     }
+    @IBAction func photoLibrary(_ sender: Any) {
+        
+    }
     
     @IBAction func capturePhoto(_ sender: UIButton) {
         // TODO: photoOutput의 capturePhoto 메소드
-
-
+        // Orientation
+        // PhotoOutput
+        
+        let videoPreviewLayerOrientation = self.previewView.videoPreviewLayer.connection?.videoOrientation
+        sessionQueue.async {
+            let connection = self.photoOutput.connection(with: .video)
+            connection?.videoOrientation = videoPreviewLayerOrientation!
+            
+            let setting = AVCapturePhotoSettings()
+            self.photoOutput.capturePhoto(with: setting, delegate: self)
+            
+            
+        }
+        
     }
     
     
     func savePhotoLibrary(image: UIImage) {
         // TODO: capture한 이미지 포토라이브러리에 저장
+        PHPhotoLibrary.requestAuthorization { (status) in
+            if status == .authorized
+            {
+                // 저장
+                PHPhotoLibrary.shared().performChanges({
+                    PHAssetChangeRequest.creationRequestForAsset(from: image)
+                }) { (success, error) in
+                    print("--> 이미지 저장 완료 했나? \(success)")
+                }
+            }
+            else {
+                // 다시 요청
+                print("error! 권한을 받지 못함")
+            }
+        }
     }
 }
+
 
 
 extension CameraViewController {
@@ -189,7 +220,7 @@ extension CameraViewController {
         // 구성 시작
         
         // ADD Video Input
-//        var defaultvideoDevice : AVCaptureDevice?
+        //        var defaultvideoDevice : AVCaptureDevice?
         guard let camera = videoDeviceDiscoverySession.devices.first else {
             captureSession.commitConfiguration()
             return
@@ -259,6 +290,11 @@ extension CameraViewController: AVCapturePhotoCaptureDelegate {
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
         // TODO: capturePhoto delegate method 구현
         
+        guard error == nil else { return }
         
+        guard let imageData = photo.fileDataRepresentation() else { return }
+        guard let image = UIImage(data: imageData) else { return }
+        
+        self.savePhotoLibrary(image: image)
     }
 }
