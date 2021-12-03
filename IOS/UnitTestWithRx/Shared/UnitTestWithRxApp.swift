0fcad3f3b7eb6da6d287ae1cf58c353895d7d03b
Jin Gyun, Jeong <jjinggu96@gmail.com>
2021-12-03 10:37:55 +0900

    ADD : New UnitTest Study Project

---
+++ IOS/UnitTestWithRx/Shared/UnitTestWithRxApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestWithRxApp.swift
+//  Shared
+//
+//  Created by 정진균 on 2021/12/03.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestWithRxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
