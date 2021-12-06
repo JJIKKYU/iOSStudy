9e3e2861d1d7578739b3db8a057aa6010c40ba29
jingyun.jeong <jjinggu96@gmail.com>
2021-12-06 15:04:29 +0900

    UPDATE : UI 세팅

--- CoreGraphics/CoreGraphicsPractice_1/CoreGraphicsPractice_1/ViewController.swift
+++ CoreGraphics/CoreGraphicsPractice_1/CoreGraphicsPractice_1/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnDrawLine(_ sender: Any) {
+    }
+    
+    @IBAction func btnDrawRectangle(_ sender: Any) {
+    }
+    
+    @IBAction func btnDrawCircle(_ sender: Any) {
+    }
+    
+    @IBAction func btnDrawArc(_ sender: Any) {
+    }
+    
+    @IBAction func btnDrawFill(_ sender: Any) {
+    }
 }
 
