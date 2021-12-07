92c8086782cb6fbc59d0cb8edb6016fbe898c138
jingyun.jeong <jjinggu96@gmail.com>
2021-12-07 09:25:28 +0900

    ADD : UnitTest 실습 프로젝트 추가

---
+++ UnitTest/UnitTestMVVM/UnitTestMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTestMVVM
+//
+//  Created by 정진균 on 2021/12/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
