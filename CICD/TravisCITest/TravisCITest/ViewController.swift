b993f134bad2dd3bb20281ef4d166868171e1117
Jin Gyun, Jeong <jjinggu96@gmail.com>
2021-12-12 14:53:47 +0900

    UPDATE : Travis 테스트를 위한 프로젝트 추가

---
+++ CICD/TravisCITest/TravisCITest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TravisCITest
+//
+//  Created by JunghunSeo on 2020/09/24.
+//  Copyright © 2020 JunghunSeo. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
